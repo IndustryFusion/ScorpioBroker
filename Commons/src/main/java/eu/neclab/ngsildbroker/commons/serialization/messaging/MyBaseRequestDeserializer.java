92e186735d8a0b45e2ad192c93fb00614595ac83
ScorpioBroker <55130839+ScorpioBroker@users.noreply.github.com>
2024-12-04 12:58:49 +0100

    wip

--- Commons/src/main/java/eu/neclab/ngsildbroker/commons/serialization/messaging/MyBaseRequestDeserializer.java
+++ Commons/src/main/java/eu/neclab/ngsildbroker/commons/serialization/messaging/MyBaseRequestDeserializer.java
@@ -31,119 +31,100 @@ import io.smallrye.mutiny.tuples.Tuple2;
 
 public class MyBaseRequestDeserializer {
 
-	private Field field;
-
 	private static final Decoder base64Decoder = Base64.getDecoder();
 	private static final TypeReference<Map<String, Object>> mapTypeRef = new TypeReference<Map<String, Object>>() {
 	};
 
-	public MyBaseRequestDeserializer() {
-		try {
-			field = String.class.getDeclaredField("value");
-			field.setAccessible(true);
-		} catch (NoSuchFieldException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SecurityException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	public static void main(String[] args) {
-
-	}
-
 	public BaseRequest deserialize(String body)
 			throws IOException, JacksonException, IllegalArgumentException, IllegalAccessException {
 		BaseRequest result = new BaseRequest();
-		final byte[] chars = (byte[]) field.get(body);
-		final int len = chars.length;
 
-		for (int i = 2; i < chars.length; i++) {
-			switch (chars[i]) {
+		final int len = body.length();
+
+		for (int i = 2; i < len; i++) {
+			switch (body.charAt(i)) {
 			case AppConstants.BYTE_ZIPPED_SERIALIZATION_CHAR: {
-				result.setZipped(chars[i + 3] == 't');
+				result.setZipped(body.charAt(i + 3) == 't');
 				i += 7;
 				break;
 			}
 			case AppConstants.BYTE_TENANT_SERIALIZATION_CHAR: {
 				int offset = i + 4;
-				int endIndex = find(chars, '"', offset);
-				result.setTenant(new String(chars, offset, endIndex - offset));
+				int endIndex = body.indexOf('"', offset);
+				result.setTenant(body.substring(offset, endIndex));
 				i += endIndex + 3;
 				break;
 			}
 
 			case AppConstants.BYTE_REQUESTTYPE_SERIALIZATION_CHAR: {
 				int offset = i + 3;
-				int endIndex = find(chars, ',', offset);
-				result.setRequestType(Integer.parseInt(new String(chars, offset, endIndex - offset)));
+				int endIndex = body.indexOf(',', offset);
+				result.setRequestType(Integer.parseInt(body.substring(offset, endIndex)));
 				i += endIndex + 2;
 				break;
 			}
 			case AppConstants.BYTE_SENDTIMESTAMP_SERIALIZATION_CHAR: {
 				int offset = i + 3;
-				int endIndex = find(chars, ',', offset);
-				result.setSendTimestamp(Long.parseLong(new String(chars, offset, endIndex - offset)));
+				int endIndex = body.indexOf(',', offset);
+				result.setSendTimestamp(Long.parseLong(body.substring(offset, endIndex)));
 				i += endIndex + 2;
 				break;
 			}
 
 			case AppConstants.BYTE_ATTRIBNAME_SERIALIZATION_CHAR: {
 				int offset = i + 4;
-				int endIndex = find(chars, '"', offset);
-				result.setAttribName(new String(chars, offset, endIndex - offset));
+				int endIndex = body.indexOf('"', offset);
+				result.setAttribName(body.substring(offset, endIndex));
 				i += endIndex + 3;
 				break;
 			}
 			case AppConstants.BYTE_DATASETID_SERIALIZATION_CHAR: {
 				int offset = i + 4;
-				int endIndex = find(chars, '"', offset);
-				result.setDatasetId(new String(chars, offset, endIndex - offset));
+				int endIndex = body.indexOf('"', offset);
+				result.setDatasetId(body.substring(offset, endIndex));
 				i += endIndex + 3;
 				break;
 			}
 			case AppConstants.BYTE_DELETEALL_SERIALIZATION_CHAR: {
-				result.setDeleteAll(chars[i + 3] == 't');
+				result.setDeleteAll(body.charAt(i + 3) == 't');
 				i += 7;
 				break;
 			}
 			case AppConstants.BYTE_DISTRIBUTED_SERIALIZATION_CHAR: {
-				result.setDistributed(chars[i + 3] == 't');
+				result.setDistributed(body.charAt(i + 3) == 't');
 				i += 7;
 				break;
 			}
 			case AppConstants.BYTE_NOOVERWRITE_SERIALIZATION_CHAR: {
-				result.setNoOverwrite(chars[i + 3] == 't');
+				result.setNoOverwrite(body.charAt(i + 3) == 't');
 				i += 7;
 				break;
 			}
 			case AppConstants.BYTE_INSTANCEID_SERIALIZATION_CHAR: {
 				int offset = i + 4;
-				int endIndex = find(chars, '"', offset);
-				result.setInstanceId(new String(chars, offset, endIndex - offset));
+				int endIndex = body.indexOf('"', offset);
+				result.setInstanceId(body.substring(offset, endIndex));
 				i += endIndex + 3;
 				break;
 			}
 			case AppConstants.BYTE_PAYLOAD_SERIALIZATION_CHAR: {
-				int end = chars.length - 2;
+				int end = body.length() - 2;
 				int start = i + 3;
 				Set<String> ids = Sets.newHashSet();
 				Map<String, List<Map<String, Object>>> payloadMap = Maps.newHashMap();
 				Map<String, List<Map<String, Object>>> prevPayloadMap = Maps.newHashMap();
 				for (int j = start; j < end; j++) {
 					int offset = j + 1;
-					int endIndex = find(chars, '"', j + 1);
+					int endIndex = body.indexOf('"', j + 1);
 
-					String id = new String(chars, offset, endIndex - offset);
+					String id = body.substring(offset, endIndex);
 					ids.add(id);
 
 					j += endIndex + 1;
-					Tuple2<Integer, Map<String, Object>> t = parsePayload(chars, j, end, result.isZipped());
+					Tuple2<Integer, Map<String, Object>> t = parsePayload(body, j, end, result.isZipped());
 					Map<String, Object> payload = t.getItem2();
 					j = t.getItem1();
-					t = parsePayload(chars, j, end, result.isZipped());
+					t = parsePayload(body, j, end, result.isZipped());
 					Map<String, Object> prevPayload = t.getItem2();
 					j = t.getItem1();
 					if (payload != null) {
@@ -168,37 +149,36 @@ public class MyBaseRequestDeserializer {
 		return result;
 	}
 
-	private Tuple2<Integer, Map<String, Object>> parsePayload(byte[] chars, int offset, int end, boolean zipped) {
+	private Tuple2<Integer, Map<String, Object>> parsePayload(String body, int offset, int end, boolean zipped) {
 
 		Map<String, Object> result;
 		int resultOffset;
 		if (zipped) {
-			if (Arrays.equals(chars, offset, MicroServiceUtils.getZippedNullArrayLength(),
-					MicroServiceUtils.getZippedNullArray(), 0, MicroServiceUtils.getZippedNullArrayLength())) {
+			if (checkForZippedNullEntry(body, offset)) {
 				result = null;
 				resultOffset = offset + MicroServiceUtils.getZippedNullArrayLength() + 1;
 			} else {
 				int b64Start = offset + 1;
-				int b64End = find(chars, '"', b64Start);
+				int b64End = body.indexOf('"', b64Start);
 				int length = b64End - b64Start;
-				byte[] tmp = new byte[length];
-				System.arraycopy(chars, b64Start, tmp, 0, length);
+				String tmp = body.substring(b64Start, b64End);
+
 				try {
-					result = parseMap(MicroServiceUtils.unzip(base64Decoder.decode(tmp)), 0, true, end).getItem2();
+					result = parseMap(new String(MicroServiceUtils.unzip(base64Decoder.decode(tmp))), 0, true, end)
+							.getItem2();
 				} catch (IOException e) {
 					result = null;
 				}
 				resultOffset = b64End + 1;
 			}
 		} else {
-			if (Arrays.equals(chars, offset, MicroServiceUtils.NULL_ARRAY.length, MicroServiceUtils.NULL_ARRAY, 0,
-					MicroServiceUtils.NULL_ARRAY.length)) {
+			if (checkForNullEntry(body, offset)) {
 				result = null;
 				resultOffset = offset + MicroServiceUtils.NULL_ARRAY.length + 1;
 			} else {
 				Tuple2<Integer, Map<String, Object>> t;
 
-				t = parseMap(chars, offset, true, end);
+				t = parseMap(body, offset, true, end);
 
 				resultOffset = t.getItem1() + 1;
 				result = t.getItem2();
@@ -208,7 +188,25 @@ public class MyBaseRequestDeserializer {
 		return Tuple2.of(resultOffset, result);
 	}
 
-	private static Tuple2<Integer, Map<String, Object>> parseMap(byte[] jsonData, int offset, boolean breakAtRoot,
+	private boolean checkForZippedNullEntry(String body, int offset) {
+		for (int i = 0; i < MicroServiceUtils.getZippedNullArrayLength(); i++) {
+			if (body.charAt(i + offset) == MicroServiceUtils.getZippedNullArray()[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private boolean checkForNullEntry(String body, int offset) {
+		for (int i = 0; i < MicroServiceUtils.NULL_ARRAY.length; i++) {
+			if (body.charAt(i + offset) == MicroServiceUtils.NULL_ARRAY[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static Tuple2<Integer, Map<String, Object>> parseMap(String jsonData, int offset, boolean breakAtRoot,
 			int end) {
 		Map<String, Object> resultMap = new HashMap<>();
 		StringBuilder keyBuilder = new StringBuilder();
@@ -219,7 +217,7 @@ public class MyBaseRequestDeserializer {
 		int i;
 
 		for (i = offset; i < end; i++) {
-			currentChar = (char) jsonData[i];
+			currentChar = jsonData.charAt(i);
 
 			if (currentChar == '{') {
 				braceCount++;
@@ -282,7 +280,7 @@ public class MyBaseRequestDeserializer {
 		return Tuple2.of(i, resultMap);
 	}
 
-	private static Tuple2<Integer, List<Object>> parseArray(byte[] jsonData, int offset, int end) {
+	private static Tuple2<Integer, List<Object>> parseArray(String jsonData, int offset, int end) {
 		List<Object> resultList = new ArrayList<>();
 		StringBuilder valueBuilder = new StringBuilder();
 		boolean inString = false;
@@ -290,8 +288,8 @@ public class MyBaseRequestDeserializer {
 		char currentChar;
 		int i;
 
-		for (i = offset; i < jsonData.length; i++) {
-			currentChar = (char) jsonData[i];
+		for (i = offset; i < end; i++) {
+			currentChar = jsonData.charAt(i);
 
 			if (currentChar == '[') {
 				braceCount++;
@@ -350,15 +348,6 @@ public class MyBaseRequestDeserializer {
 				return value; // Return as string if not a number
 			}
 		}
-	}
-
-	private int find(byte[] chars, char c, int offset) {
-		for (int i = offset; i < chars.length; i++) {
-			if (chars[i] == c) {
-				return i;
-			}
-		}
-		return -1;
 	}
 
 }
